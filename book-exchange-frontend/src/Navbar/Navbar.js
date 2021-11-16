ba156939cacc91137996dbe30f541d4da60625fa
saltyfedor <fedor.stryapunin@gmail.com>
2021-11-16 21:14:12 +0100

    installed styled components, added simple styling to navbar

--- book-exchange-frontend/src/Navbar/Navbar.js
+++ book-exchange-frontend/src/Navbar/Navbar.js
@@ -1,12 +1,26 @@
 import React from "react";
 import GoogleAuth from "../Auth/GoogleAuth";
+import styled from 'styled-components'
+
+const StyledNavbar = styled.div`
+padding: 20px;
+box-sizing: border-box;
+width: 100%;
+display : flex;
+justify-content: flex-end;
+border-bottom: 2px solid var(--dark-blue);
+`
 
 const Navbar = ({ isLogged, onLogin, onLogout }) => {
     return (
-        <div>
+      
+        <StyledNavbar>
             <GoogleAuth isLogged={isLogged} onLogin={onLogin} onLogout={onLogout}/>
-        </div>
+        </StyledNavbar>
+      
     )
 }
 
 export default Navbar
+
+/* <GoogleAuth isLogged={isLogged} onLogin={onLogin} onLogout={onLogout}/>*/
