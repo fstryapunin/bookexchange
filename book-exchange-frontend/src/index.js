e30b3c6f3d3dd3c823eb0c53c6cccaadc8ac6192
saltyfedor <fedor.stryapunin@gmail.com>
2021-11-16 14:12:36 +0100

    added blank google auth component

--- book-exchange-frontend/src/index.js
+++ book-exchange-frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GoogleAuth from './GoogleAuth';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GoogleAuth/>
   </React.StrictMode>,
   document.getElementById('root')
 );
