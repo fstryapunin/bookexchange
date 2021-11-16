24ff3c8ddb082143d3ae12364ff71983cfde58cc
saltyfedor <fedor.stryapunin@gmail.com>
2021-11-16 16:17:30 +0100

    added server endpoint to handle google login

--- book-exchange-backend/server.js
+++ book-exchange-backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { OAuth2Client } = require('google-auth-library')
 const { port, googleClientId } = require('./config');
 
+
 const app = express();
+const googleClient = new OAuth2Client(googleClientId)
 
 app.use(
     express.json() 
@@ -14,4 +17,20 @@ console.log('Listening on port', port)
 
 app.get('/', (req, res) => {
     res.sendStatus(200)
+})
+
+app.post('/auth/google/login', async (req, res) => {
+    
+    const { token } = req.body
+    if (token) {
+        const ticket = await googleClient.verifyIdToken({
+            idToken: token,
+            audience: googleClientId
+        });
+           
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(400)
+    }
+
 })
