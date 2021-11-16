38a8581a9420b73ee1e8f86cde616fc4d02d0202
saltyfedor <fedor.stryapunin@gmail.com>
2021-11-16 14:44:04 +0100

    added login with google button

--- book-exchange-frontend/src/GoogleAuth.js
+++ book-exchange-frontend/src/GoogleAuth.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { GoogleLogin } from 'react-google-login'
+
+const clientId = '1028693232863-ahvojojs4rkgl1qv1op7q5010mk6hilk.apps.googleusercontent.com'
 
 const GoogleAuth = () => {
+
+    const onLogin = (res) => {
+        console.log('Login response')
+        console.log(res)
+    }
+
     return (
         <div>
             <h1>Hello world</h1>
+            <GoogleLogin
+                clientId={clientId}
+                buttonText="Login"
+                onSuccess={onLogin}
+                onFailure={onLogin}
+                cookiePolicy={'single_host_origin'}
+                isSignedIn={true}
+            />
         </div>
     )
 }
