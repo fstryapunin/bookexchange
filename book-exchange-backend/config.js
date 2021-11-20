e9de49427f3559037f2177954c5d0d3c582bf3b2
saltyfedor <fedor.stryapunin@gmail.com>
2021-11-20 23:08:02 +0100

    installed cookie parser to handle jwt token cookies

--- book-exchange-backend/config.js
+++ book-exchange-backend/config.js
@@ -3,5 +3,6 @@ dotenv.config();
 module.exports = { 
     port: process.env.PORT,
     googleClientId: process.env.GOOGLE_CLIENT_ID,
-    dbUrl: process.env.DATABASE_URL 
+    dbUrl: process.env.DATABASE_URL,
+    tokenKey: process.env.TOKEN_KEY
 };
--- book-exchange-backend/server.js
+++ book-exchange-backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const knex = require('knex')
 const cors = require('cors')
+const jwt = require("jsonwebtoken");
 const { OAuth2Client } = require('google-auth-library')
-const { port, googleClientId, dbUrl } = require('./config');
+const { port, googleClientId, dbUrl, tokenKey } = require('./config');
 
 
 const app = express();
@@ -34,6 +35,33 @@ app.get('/', (req, res) => {
     res.sendStatus(200)
 })
 
+const upsertUser = async (payload) => {
+    const firstName = payload.given_name
+    const lastName = payload.family_name
+    const email = payload.email
+    const imageLink = payload.picture
+
+    const exists = await db.select('*').from('users').where('email', email)
+        if (exists.length === 0) {
+            const newUserId = await db('users').insert({
+                email: email,
+                first_name: firstName,
+                last_name: lastName,
+                img_link: imageLink
+            }, 'id')
+            return newUserId
+        } else {
+            const userId = await db('users').where('email', email).update({
+                email: email,
+                first_name: firstName,
+                last_name: lastName,
+                img_link: imageLink
+            }, 'id')
+
+            return userId
+        }
+}
+
 app.post('/auth/google/login', async (req, res) => {
     
     const { token } = req.body   
@@ -46,32 +74,21 @@ app.post('/auth/google/login', async (req, res) => {
         });
         
         const payload = ticket.getPayload()
-        console.log(payload)
-        const firstName = payload.given_name
-        const lastName = payload.family_name
-        const email = payload.email
-        const imageLink = payload.picture
-
-        const exists = await db.select('*').from('users').where('email', email)
-        if (exists.length === 0) {
-            const newUserId = await db('users').insert({
-                email: email,
-                first_name: firstName,
-                last_name: lastName,
-                img_link: imageLink
-            }, 'id')
-            res.sendStatus(200)
-        } else {
-            const userId = await db('users').where('email', email).update({
-                email: email,
-                first_name: firstName,
-                last_name: lastName,
-                img_link: imageLink
-            }, 'id')
-
-            res.sendStatus(200)
-        }
         
+        const userId = upsertUser(payload)      
+
+        const jToken = jwt.sign(
+            {
+                email: payload.email,
+                id : userId
+            },
+            tokenKey,
+            {
+              expiresIn: "1m",
+            }
+        );       
+
+        res.status(200).json(jToken)      
         
     } else {
         res.sendStatus(400)
