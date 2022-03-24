5ff64a813859d455726d06fbe4365605087c37cc
saltyfedor <fedor.stryapunin@gmail.com>
2022-03-24 01:51:49 +0100

    removed useless imports

--- book-exchange-frontend/src/Listings/listingsSlice.js
+++ book-exchange-frontend/src/Listings/listingsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, current  } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk  } from '@reduxjs/toolkit'
 const apiAdress = process.env.REACT_APP_API_ADRESS
 
 export const fetchListings = createAsyncThunk('listings/get', async (page = 0) => {
--- book-exchange-frontend/src/Messages/MessageInput.js
+++ book-exchange-frontend/src/Messages/MessageInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Send } from '@styled-icons/boxicons-solid/Send'
 
